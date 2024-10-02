b9b1398c6384245943d045ba25e8d9186719fae0
s19013 <s19013@std.it-college.ac.jp>
2024-10-02 16:35:05 +0900

    カスタムルーティングファイルを紐づける

--- laravel/bootstrap/app.php
+++ laravel/bootstrap/app.php
@@ -3,12 +3,20 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Support\Facades\Route;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
+        then: function () {
+            // ここに各時点でデフォルトミドルウェアグループを適応させるのを忘れないように
+            Route::middleware('web')
+                ->prefix('warehouse')
+                ->name('warehouse.')
+                ->group(base_path('routes/warehouse/web.php'));
+        },
     )
     ->withMiddleware(function (Middleware $middleware) {
         //

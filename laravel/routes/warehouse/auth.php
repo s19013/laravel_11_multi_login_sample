663e7223bdd58ea6b3c8af750f49ea8abafa10e8
s19013 <s19013@std.it-college.ac.jp>
2024-10-02 16:39:20 +0900

    パス変更

--- laravel/routes/warehouse/auth.php
+++ laravel/routes/warehouse/auth.php
@@ -1,14 +1,14 @@
 <?php
 
-use App\Http\Controllers\Auth\AuthenticatedSessionController;
-use App\Http\Controllers\Auth\ConfirmablePasswordController;
-use App\Http\Controllers\Auth\EmailVerificationNotificationController;
-use App\Http\Controllers\Auth\EmailVerificationPromptController;
-use App\Http\Controllers\Auth\NewPasswordController;
-use App\Http\Controllers\Auth\PasswordController;
-use App\Http\Controllers\Auth\PasswordResetLinkController;
-use App\Http\Controllers\Auth\RegisteredUserController;
-use App\Http\Controllers\Auth\VerifyEmailController;
+use App\Http\Controllers\Warehouse\Auth\AuthenticatedSessionController;
+use App\Http\Controllers\Warehouse\Auth\ConfirmablePasswordController;
+use App\Http\Controllers\Warehouse\Auth\EmailVerificationNotificationController;
+use App\Http\Controllers\Warehouse\Auth\EmailVerificationPromptController;
+use App\Http\Controllers\Warehouse\Auth\NewPasswordController;
+use App\Http\Controllers\Warehouse\Auth\PasswordController;
+use App\Http\Controllers\Warehouse\Auth\PasswordResetLinkController;
+use App\Http\Controllers\Warehouse\Auth\RegisteredUserController;
+use App\Http\Controllers\Warehouse\Auth\VerifyEmailController;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware('guest:warehouse')->group(function () {

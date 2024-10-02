bf21039a9a7142ee5adada882158b9770ad10207
s19013 <s19013@std.it-college.ac.jp>
2024-10-02 16:50:43 +0900

    ガード適応、パス変更

--- laravel/app/Http/Controllers/Warehouse/Auth/RegisteredUserController.php
+++ laravel/app/Http/Controllers/Warehouse/Auth/RegisteredUserController.php
@@ -19,7 +19,7 @@ class RegisteredUserController extends Controller
      */
     public function create(): View
     {
-        return view('auth.register');
+        return view('warehouse.auth.register');
     }
 
     /**
@@ -43,8 +43,8 @@ class RegisteredUserController extends Controller
 
         event(new Registered($user));
 
-        Auth::login($user);
+        Auth::guard('warehouse')->login($user);
 
-        return redirect(route('dashboard', absolute: false));
+        return redirect(route('warehouse.dashboard', absolute: false));
     }
 }

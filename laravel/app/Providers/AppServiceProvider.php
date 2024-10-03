fa90d20984ca59c9a56aec07bb804fe3eb39d55a
s19013 <s19013@std.it-college.ac.jp>
2024-10-03 19:05:52 +0900

    appServiceProviderに書くよりミドルウェア化してグローバルミドルウェアに加えたほうが副作用が少ないらしい。

--- laravel/app/Providers/AppServiceProvider.php
+++ laravel/app/Providers/AppServiceProvider.php
@@ -19,14 +19,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        // ページによって使うセッションを変える
-        // お互いのページには影響がないため大丈夫らしい(情報が少ないため不安が残る)
-        if (request()->is('warehouse/*')) {
-            config(['session.cookie' => config('session.warehouse_cookie')]);
-        }
 
-        if (request()->is('agency/*')) {
-            config(['session.cookie' => config('session.agency_cookie')]);
-        }
     }
 }
--- laravel/bootstrap/app.php
+++ laravel/bootstrap/app.php
@@ -4,6 +4,7 @@ use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 use Illuminate\Support\Facades\Route;
+use App\Http\Middleware\SessionCookieSwitcher;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -24,6 +25,8 @@ return Application::configure(basePath: dirname(__DIR__))
         },
     )
     ->withMiddleware(function (Middleware $middleware) {
+        $middleware->append(SessionCookieSwitcher::class);
+
         $middleware->redirectUsersTo(function ($request) {
             // 倉庫系のurlでログイン済みの場合、未ログインユーザーのみアクセスできるページにアクセスしようとすると弾く
             if ($request->is('warehouse*')) {

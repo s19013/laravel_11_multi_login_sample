c8b186fc8d8a1cd4cae07f95f5498725a7a02c17
s19013 <s19013@std.it-college.ac.jp>
2024-10-03 11:16:22 +0900

    モデルにパスワードリセットクラスを紐づけ

--- laravel/app/Models/WarehouseUser.php
+++ laravel/app/Models/WarehouseUser.php
@@ -7,9 +7,18 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
+use App\Notifications\Warehouse\ResetPassword;
+
+
 class WarehouseUser extends Authenticatable
 {
     use HasFactory, Notifiable;
+
+    // パスワードリセットで使うクラスをオーバーライドで変更
+    public function sendPasswordResetNotification($token)
+    {
+        $this->notify(new ResetPassword($token));
+    }
 
     /**
      * The attributes that are mass assignable.

c07781a0218e6ee8fec3a37c89a47cac0307e2a2
s19013 <s19013@std.it-college.ac.jp>
2024-10-03 16:46:17 +0900

    参照するパスワードリセットクラスが間違っていた

--- laravel/app/Models/AgencyUser.php
+++ laravel/app/Models/AgencyUser.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
-use App\Notifications\Warehouse\ResetPassword;
+use App\Notifications\Agency\ResetPassword;
 
 class AgencyUser extends Authenticatable
 {

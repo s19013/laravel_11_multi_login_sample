8e69f7441b51e1500e88a780ed82b3213d0faed3
s19013 <s19013@std.it-college.ac.jp>
2024-10-03 15:37:41 +0900

    auth.phpで、プロパティ名などを変えたのでブローカーで参照先変更

--- laravel/app/Http/Controllers/Warehouse/Auth/PasswordResetLinkController.php
+++ laravel/app/Http/Controllers/Warehouse/Auth/PasswordResetLinkController.php
@@ -32,7 +32,9 @@ class PasswordResetLinkController extends Controller
         // We will send the password reset link to this user. Once we have attempted
         // to send the link, we will examine the response then see the message we
         // need to show to the user. Finally, we'll send out a proper response.
-        $status = Password::sendResetLink(
+
+        // brokerを使って参照するテーブルを変更
+        $status = Password::broker('warehouse_users')->sendResetLink(
             $request->only('email')
         );
 

92ff2de0c9d23b34b7c2a65c84d6bf4e6719e5c7
s19013 <s19013@std.it-college.ac.jp>
2024-10-03 16:30:51 +0900

    リセット機能編集

--- laravel/app/Models/AgencyUser.php
+++ laravel/app/Models/AgencyUser.php
@@ -7,9 +7,17 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
+use App\Notifications\Warehouse\ResetPassword;
+
 class AgencyUser extends Authenticatable
 {
     use HasFactory, Notifiable;
+
+    // パスワードリセットで使うクラスをオーバーライドで変更
+    public function sendPasswordResetNotification($token)
+    {
+        $this->notify(new ResetPassword($token));
+    }
 
     /**
      * The attributes that are mass assignable.

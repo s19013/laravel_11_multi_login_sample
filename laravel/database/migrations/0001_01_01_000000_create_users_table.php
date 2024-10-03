7072e32aada87fa2a671f3c513f0905f0a889725
s19013 <s19013@std.it-college.ac.jp>
2024-10-03 16:01:12 +0900

    代理店テーブル追加

--- laravel/database/migrations/0001_01_01_000000_create_users_table.php
+++ laravel/database/migrations/0001_01_01_000000_create_users_table.php
@@ -27,6 +27,22 @@ return new class extends Migration
             $table->timestamp('created_at')->nullable();
         });
 
+        Schema::create('agency_users', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('email')->unique();
+            $table->timestamp('email_verified_at')->nullable();
+            $table->string('password');
+            $table->rememberToken();
+            $table->timestamps();
+        });
+
+        Schema::create('agency_user_password_reset_tokens', function (Blueprint $table) {
+            $table->string('email')->primary();
+            $table->string('token');
+            $table->timestamp('created_at')->nullable();
+        });
+
         Schema::create('sessions', function (Blueprint $table) {
             $table->string('id')->primary();
             $table->foreignId('user_id')->nullable()->index();

eaeb84883a7bee7342870f601b052631fd7ca84b
s19013 <s19013@std.it-college.ac.jp>
2024-10-04 10:59:53 +0900

    urlが存在しなかったときの処理

--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+
+
 
 Route::get('/', function () {
     return view('welcome');
@@ -15,4 +18,16 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+Route::fallback(function (Request $request) {
+    if ($request->is('warehouse/*')){
+        return redirect(route('warehouse.test'));
+    }
+
+    if ($request->is('agency/*')){
+        return redirect(route('agency.test'));
+    }
+
+    return redirect('/');
 });

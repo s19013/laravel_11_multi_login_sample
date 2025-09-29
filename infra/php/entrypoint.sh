cad90ac01ac140a134c10dc34a5e94c43bacc06a
hide <50346558+s19013@users.noreply.github.com>
2025-09-29 19:06:40 +0900

    Update entrypoint.sh

--- infra/php/entrypoint.sh
+++ infra/php/entrypoint.sh
@@ -6,7 +6,7 @@ INIT_FLAG="/laravel/.init_done"
 # フラグファイルが存在しない場合にのみ初期化処理を実行
 if [ ! -f "$INIT_FLAG" ];then
     echo "Running initial setup..."
-    bash /laravel/init.sh
+    bash "/laravel/init.sh"
     # 初回実行後にフラグファイルを作成
     touch "$INIT_FLAG"
 else
